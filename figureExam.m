161939c763d9ac7554e12566f0fe96b9c2649555
lythanhhai <you@example.com>
2021-10-11 23:02:45 +0700

    update AMDF

--- figureExam.m
+++ figureExam.m
@@ -1,4 +1,4 @@
-Fo_studio_fe = AMDF1('./fileTinHieuMoi/studio_F1.wav',1);
-Fo_studio_ma = AMDF1('./fileTinHieuMoi/studio_M1.wav',2);
-Fo_phone_fe = AMDF1('./fileTinHieuMoi/phone_F1.wav',3);
-Fo_phone_ma = AMDF1('./fileTinHieuMoi/phone_M1.wav',4);
+Fo_studio_fe = AMDF1('./fileTinHieuMoi/studio_F1.wav',1,30,41);
+Fo_studio_ma = AMDF1('./fileTinHieuMoi/studio_M1.wav',2,35,30);
+Fo_phone_fe = AMDF1('./fileTinHieuMoi/phone_F1.wav',3,25,39);
+Fo_phone_ma = AMDF1('./fileTinHieuMoi/phone_M1.wav',4, 25,48);
